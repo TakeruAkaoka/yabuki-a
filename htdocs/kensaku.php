ce0d040c18cafe060d95d3dc7440b1526979ae15
TakeruAkaoka <s1542002kn@s.chibakoudai.jp>
2017-07-21 11:09:29 +0900

    Update kensaku.php

--- htdocs/kensaku.php
+++ htdocs/kensaku.php
@@ -46,11 +46,7 @@ if(empty($_POST)) {
 
 if(count($errors) === 0){
 
-<<<<<<< HEAD
-	$dsn = 'mysql:host=localhost;dbname=kensaku;charset=utf8';
-	$user = 'k';
-	$password = '12345';
-=======
+
 require_once 'database_conf.php';
 
 	//$dsn = 'mysql:host=localhost;dbname=kensaku;charset=utf8';
